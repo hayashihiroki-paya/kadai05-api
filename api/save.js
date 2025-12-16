dcb21aaa60f24202039ab316b7743e7215768f60
hayashihiroki-paya <hayashihiroki517@gmail.com>
2025-12-16 15:44:29 +0900

    またCORSの微修正

--- api/save.js
+++ api/save.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
   // ===== CORS ヘッダー =====
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
   // ===== preflight 対応 =====
   if (req.method === "OPTIONS") {
